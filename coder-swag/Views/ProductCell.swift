ac4198ee5a507494613f57f5cf9b51ae9b533923
Andy Taylor <Theador0691@gmail.com>
2017-09-09 18:09:40 +0100

    Started to setup the products page and started to lay out the collection view cell

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Andrew Taylor on 09/09/2017.
+//  Copyright © 2017 Andrew Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+
+    
+}
